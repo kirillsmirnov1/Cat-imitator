b55074b94e86a922e807990a45699e8cce5d745f
Kirill Smirnov <thegaldren@gmail.com>
2020-08-10 17:31:54 +0300

    basic button creation

---
+++ Assets/__Project/Scripts/ActionButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionButton : MonoBehaviour
+{
+    private Text _text;
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            _text.text = _name;
+        }
+    }
+
+    private void Awake()
+    {
+        _text = GetComponentInChildren<Text>();
+    }
+}
---
+++ Assets/__Project/Scripts/ActionButtons.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(HorizontalLayoutGroup))]
+public class ActionButtons : MonoBehaviour
+{
+    public string[] buttonNames;
+    public GameObject actionButtonPrefab;
+
+    private void Awake()
+    {
+        foreach (var buttonName in buttonNames)
+        {
+            var button = Instantiate(actionButtonPrefab, transform).GetComponent<ActionButton>();
+            button.Name = buttonName;
+        }
+    }
+}
