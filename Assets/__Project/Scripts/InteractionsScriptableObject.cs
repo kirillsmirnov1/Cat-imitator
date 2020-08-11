2b0ef047ebfa3c67e9bcd946560e0be9eccd7622
Kirill Smirnov <thegaldren@gmail.com>
2020-08-11 17:21:34 +0300

    Add InteractionsScriptableObject

---
+++ Assets/__Project/Scripts/InteractionsScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/InteractionsSO")]
+public class InteractionsScriptableObject : ScriptableObject
+{
+    public InteractionData[] interactions;
+    
+    [Serializable]
+    public struct InteractionData
+    {
+        public InteractionType type;
+        public string label;
+        public GameObject particleSystemPrefab;
+    }
+}
