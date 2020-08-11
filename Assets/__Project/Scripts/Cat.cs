a67789ecd5330671314fd41120f4b4705230b0a7
Kirill Smirnov <thegaldren@gmail.com>
2020-08-11 09:22:08 +0300

    interaction sets trigger in cat's animation controller

--- Assets/__Project/Scripts/Cat.cs
+++ Assets/__Project/Scripts/Cat.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Cat : MonoBehaviour
 {
     public static Cat Instance;
 
+    private Animator _animator;
+    
     private void Awake()
     {
         Instance = this;
+        _animator = GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Apply interaction to Cat
+    /// </summary>
     public void Interact(InteractionType interaction)
     {
         Debug.Log($"Interaction: {interaction}");
+        _animator.SetTrigger(interaction.ToString());
     }
 }
