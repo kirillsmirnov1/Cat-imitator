33523972d9526d9b9b27c792834a4a5a854b3b7a
Kirill Smirnov <thegaldren@gmail.com>
2020-08-11 16:18:37 +0300

    Add preloader scene

---
+++ Assets/__Project/Scripts/Preloader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Preloader : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadMainScene());
+        }
+    }
+
+    private IEnumerator LoadMainScene()
+    {
+        yield return SceneManager.LoadSceneAsync("MainScene");
+    }
+}
